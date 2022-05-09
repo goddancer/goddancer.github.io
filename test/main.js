c94b23e636879e533dcfedb9a9a2cd8a57d42c2f
goddancer <goddancer@foxmail.com>
2022-05-09 21:53:51 +0800

    upd: up

--- test/main.js
+++ test/main.js
@@ -1,18 +1,11 @@
-var lengthOfLIS = function (nums) {
-  if (!nums.length) {
-    return 0
-  }
-  const dp = new Array(nums.length).fill(-1)
-  dp[0] = 1
-  for (let i = 1; i <= nums.length; i++) {
-    const temp = [1]
-    for (let j = i - 1; j >= 0; j--) {
-      if (nums[j] < nums[i] && dp[j] !== -1) {
-        temp.push(dp[j] + 1)
-      }
-    }
-    temp.length && (dp[i] = Math.max(...temp))
-  }
-  return Math.max(...dp)
-};
-console.log(lengthOfLIS([7,7,7,7]))
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val)
+  this.next = (next === undefined ? null : next)
+}
+let temp = new ListNode()
+temp.val = 1
+console.log('temp: ', temp);
+
+let obj = Object.create(ListNode.prototype)
+obj.val = 0
+obj.next = null
